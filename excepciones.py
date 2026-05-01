220aeb5ef6be1efdbab7e91a2315bd1a6d8114ca
Alejandroceballos95 <alejandro.ceballos95@icloud.com>
2026-05-01 11:43:21 -0500

    Creación de excepciones personalizadas

--- excepciones.py
+++ excepciones.py
@@ -1,3 +1,25 @@
-# ESPACIO PARA LA OPCIÓN 4 (QA y Excepciones)
-# Aquí se deben crear las cxlases de excepciones personalizadas.
-# Ejemplo: class ClienteNoValidoError(Exception):
+class SistemaGestionError(Exception):
+    """
+    Clase base fundamental para nuestro sistema.
+    Todas las demás excepciones heredarán de este lugar de la genérica de Python.
+    """
+
+
+class DatoInvalidoError(SistemaGestionError):
+    """
+    Lanzada cuando un dato ingresado (como el ID del cliente) no cumpleel formato.
+    """
+
+    def __init__(self, mensaje="Error de validación: El dato ingresado es incorrecto."):
+        self.mensaje = mensaje
+        super().__init__(self.mensaje)
+
+
+class RecursoNoDisponibleError(SistemaGestionError):
+    """
+    Lanzada cuando se intenta hacer una acción ilógica (ej: cancelar una reserva ya cancelada).
+    """
+
+    def __init__(self, mensaje="Error de operación: Acción no permitida en este estado."):
+        self.mensaje = mensaje
+        super().__init__(self.mensaje)
